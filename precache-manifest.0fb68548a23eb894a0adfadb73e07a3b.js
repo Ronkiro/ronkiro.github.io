084f54c3e5ee13e6a0d614bd6ac73b9986a39542
Alexander Santos <dev_alex@outlook.com>
2019-12-23 21:26:09 -0300

    Updates

---
+++ precache-manifest.0fb68548a23eb894a0adfadb73e07a3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "929cb68606f8b839c2d4",
+    "url": "/static/css/main.9a33be41.chunk.css"
+  },
+  {
+    "revision": "929cb68606f8b839c2d4",
+    "url": "/static/js/main.929cb686.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d00271801c9bb0eefc1a9ce5ea53c2c",
+    "url": "/index.html"
+  }
+];
