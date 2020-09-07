07cf06ede6a38f56a7fc86235f8241b5b1b72f3f
Alexander Santos <dev_alex@outlook.com>
2020-09-07 05:56:00 -0300

    :sparkles: add proj

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,34 +5,39 @@ export default class Porfolio extends Component {
     let portfolioData = this.props.portfolioData;
     return (
       <section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Dê uma olhada em alguns dos meus trabalhos.</h1>
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-          {
-            portfolioData.portfolio && portfolioData.portfolio.map((item)=>{
-              return(
-                <div className="columns portfolio-item">
-                  <div className="item-wrap item-size">
-                    <a href={ `${item.imgurl}` }>
-                      <img alt="Project" src={`${item.imgurl}`} className="item-img"/>
-                      <div className="overlay">
-                        <div className="portfolio-item-meta">
-                          <h5>{item.name}</h5>
-                          <p>{item.description}</p>
-                          <a className="projlink" href={item.link}>Link do Projeto</a>
-                        </div>
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Dê uma olhada em alguns dos meus trabalhos.</h1>
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+              {
+                portfolioData.portfolio && portfolioData.portfolio.map((item) => {
+                  return (
+                    <div className="columns portfolio-item">
+                      <div className="item-wrap item-size">
+                        <a onClick={this.stopPropag} href={`${item.imgurl}`}>
+                          <img alt="Project" src={`${item.imgurl}`} style={{
+                            position: "absolute",
+                            left: "50%",
+                            top: "50%",
+                            transform: "translate(-50%, -50%)"
+                          }}
+                            className="item-img" />
+                          <div className="overlay">
+                            <div className="portfolio-item-meta">
+                              <h5>{item.name}</h5>
+                              <p>{item.description}</p>
+                            </div>
+                          </div>
+                        </a>
                       </div>
-                    </a>
-                  </div>
-                </div>
-              )
-            })
-          }
+                    </div>
+                  )
+                })
+              }
+            </div>
           </div>
         </div>
-      </div>
-  </section>
-        );
+      </section>
+    );
   }
 }
